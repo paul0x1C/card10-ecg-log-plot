dc892303e3c164cc716cfaf9103a35ba2631535a
paul0x1C <p4ul@posteo.net>
2019-09-05 01:49:51 +0200

    remove unnecessary iteration

--- plot.py
+++ plot.py
@@ -1,13 +1,10 @@
 import matplotlib.pylab as plt
 from struct import iter_unpack
 
-y = []
 file = "ecg.log"
 
 with open(file, "rb") as f:
-	bytes = f.read()
-	for value in iter_unpack("h", bytes):
-		y.append(value)
+	y = list(iter_unpack("h", f.read()))
 
 plt.plot(y)
 plt.show()
